edf750518232dc0f8d12b69f8645bb9876da4217
iulia.filip <iulia.filip@newton.ro>
2025-05-02 13:34:42 +0300

    add  domain and data file structuring for signup feature

---
+++ lib/data/repository/auth.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:inteleqt_quiz/domain/repository/auth.dart';
+
+class AuthRepositoryImpl extends AuthRepository {
+  @override
+  Future<Either> signup() {
+    // TODO: implement signup
+    throw UnimplementedError();
+  }
+}
